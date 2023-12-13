cbc2246ea8cb0c50701cfbf953708ca8d52c236b
Cassius Adriano Fiorin <c.fiorin@samsung.com>
2023-12-13 09:27:49 -0300

    Add project files.

---
+++ TrucoPaulista.h
@@ -0,0 +1,32 @@
+
+// TrucoPaulista.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CTrucoPaulistaApp:
+// See TrucoPaulista.cpp for the implementation of this class
+//
+
+class CTrucoPaulistaApp : public CWinApp
+{
+public:
+	CTrucoPaulistaApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTrucoPaulistaApp theApp;
