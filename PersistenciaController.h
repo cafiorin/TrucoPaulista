b94fc624492110b9969b387f79c572079226a391
Cassius Adriano Fiorin <c.fiorin@samsung.com>
2024-01-10 23:17:42 -0300

    comentado o codigo da Persistencia para resolver o problema da lib do json

--- PersistenciaController.h
+++ PersistenciaController.h
@@ -1,22 +1,22 @@
-#pragma once
-#include "Carta.h"
-#include "Rodada.h"
-
-class PersistenciaController
-{
-public:
-	const std::string nomeArquivo = "trucoPaulista.json";
-
-	bool ContinuarJogoPausado();
-
-	void CriarArquivo();
-
-	void RemoverArquivo();
-
-	void AtualizarArquivo();
-
-private:
-	
-	std::string MontarJSON();
-	void PersistirJSON(std::string &json);
-};
+//#pragma once
+//#include "Carta.h"
+//#include "Rodada.h"
+//
+//class PersistenciaController
+//{
+//public:
+//	const std::string nomeArquivo = "trucoPaulista.json";
+//
+//	bool ContinuarJogoPausado();
+//
+//	void CriarArquivo();
+//
+//	void RemoverArquivo();
+//
+//	void AtualizarArquivo();
+//
+//private:
+//	
+//	std::string MontarJSON();
+//	void PersistirJSON(std::string &json);
+//};
